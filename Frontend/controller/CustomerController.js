ab955048457d9247259b15df73ca38e536e6d82d
BimalshaHasanthi <bimalshahasanthi@gmail.com>
2022-07-01 02:25:21 -0700

    Added customer search and other functions

--- Frontend/controller/CustomerController.js
+++ Frontend/controller/CustomerController.js
@@ -1,4 +1,4 @@
-const customerIdRegEx = /^(C00-)[0-9]{3,4}$/;
+/* const customerIdRegEx = /^(C00-)[0-9]{3,4}$/;
 const customerNameRegEx = /^[A-z ]{2,20}$/;
 const customerAddressRegEx = /^[A-z0-9/,. ]*$/;
 const customerSalaryRegEx = /^[0-9]*(.)[0-9]{2}$/;
@@ -365,6 +365,240 @@ function isCustomerExists(id){
     return false;
 }
 
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+const cusIDRegEx = /^(C-)[0-9]{5}$/;
+const cusNameRegEx = /^[A-z ]{2,20}$/;
+const cusAddressRegEx = /^[0-9/A-z. ,]{5,}$/;
+const cusContactRegEx = /^(0)[0-9]{2}(-)[0-9]{7}$/;
+
+let btnCusSearch = $("#btnSearchCustomer");
+let btnCusSave = $("#btnAddCustomer");
+let btnCusUpdate = $("#btnUpdateCustomer");
+let btnCusDelete = $("#btnDeleteCustomer");
+
+let txtCusSearch = $("#txtSearchCustomerId");
+let txtCusID = $("#txtCustomerId");
+let txtCusName = $("#txtCustomerName");
+let txtCusAddress = $("#txtCustomerAddress");
+let txtCusSalary = $("#txtCustomerSalary");
+
+let cmbOrderCusId = $("#cmbOrderCustId");
+let tblCustomer = $("#customerTbl");
+
+
+
+txtCusID.keyup(function (event) {
+    validateCustId();
+    if (event.key === 'Enter' && cusIDRegEx.test(txtCusID.val())){
+        txtCusName.focus();
+    }
+});
+txtCusName.keyup(function (event) {
+    validateCustName();
+    if (event.key === 'Enter' && cusNameRegEx.test(txtCusName.val())){
+        txtCusAddress.focus();
+    }
+});
+txtCusAddress.keyup(function (event) {
+    validateCustAddress();
+    if (event.key === 'Enter' && cusAddressRegEx.test(txtCusAddress.val())){
+        txtCusContact.focus();
+    }
+});
+txtCusSalary.keyup(function (event) {
+    validateCustContact();
+});
+
+
+
+$(document).ready(function() {
+    loadAllCustomers();
+    loadFromCustomerTable();
+    setCustomerCombo();
+});
+
+btnCusSearch.click(function () {
+    clearAllCustomerFields();
+
+    $.ajax({
+        url:"http://localhost:8080/Backend/customer?option=SEARCH&id="+txtCusSearch.val(),
+        method:"GET",
+        contentType:"application/json",
+        success:function (jsonResp) {
+            if(jsonResp.status===200){
+                loadCustomerToFields(jsonResp.data);
+            }else if(jsonResp.status===404){
+                alert(jsonResp.message);
+            }else{
+                alert(jsonResp.data);
+            }
+        },
+        error:function (ob, textStatus, error) {
+            console.log(ob);
+            console.log(textStatus);
+            console.log(error);
+        }
+    });
+
+    function loadCustomerToFields(data) {
+        txtCusID.val(data.id);
+        txtCusName.val(data.name);
+        txtCusAddress.val(data.address);
+        txtCusSalary.val(data.salary);
+
+        validateCustId();
+        validateCustName();
+        validateCustAddress();
+        validateCustContact();
+    }
+
+    loadFromCustomerTable();
+
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function loadFromCustomerTable() {
+
+    $("#customerTbl>tr").click(function () {
+        let id = $(this).children(":eq(0)").text();
+        let name = $(this).children(":eq(1)").text();
+        let address = $(this).children(":eq(2)").text();
+        let salary = $(this).children(":eq(3)").text();
+
+        console.log(id, name, address, salary);
+
+        txtCusID.val(id);
+        txtCusName.val(name);
+        txtCusAddress.val(address);
+        txtCusSalary.val(salary);
+
+        validateCustId();
+        validateCustName();
+        validateCustAddress();
+        validateCustContact();
+    });
+}
+
+function setCustomerCombo() {
+    cmbOrderCusId.empty();
+    cmbOrderCusId.append(new Option("Customer ID", ""));
+
+    $.ajax({
+        url:"http://localhost:8080/Backend/place-order?option=GET-ALL-CUSTOMER-IDS",
+        method:"GET",
+        contentType:"application/json",
+        success:function (jsonResp) {
+            if(jsonResp.status===200){
+                for (let i=0; i<jsonResp.data.length; i++) {
+                    let id=jsonResp.data[i].id;
+                    cmbOrderCusId.append(new Option(id, id));
+                }
+            }else if(jsonResp.status===404){
+                alert(jsonResp.message);
+            }else{
+                alert(jsonResp.data);
+            }
+        },
+        error:function (ob, textStatus, error) {
+            console.log(ob);
+            console.log(textStatus);
+            console.log(error);
+        }
+    });
+
+}
+
+function clearAllCustomerFields() {
+    txtCusID.val('');
+    txtCusName.val('');
+    txtCusAddress.val('');
+    txtCusSalary.val('');
+
+    txtCusID.css('border','1px solid #ced4da');
+    txtCusName.css('border','1px solid #ced4da');
+    txtCusAddress.css('border','1px solid #ced4da');
+    txtCusSalary.css('border','1px solid #ced4da');
+}
+
+function loadAllCustomers() {
+
+    $.ajax({
+        url:"http://localhost:8080/Backend/customer?option=GET-ALL",
+        method:"GET",
+        contentType:"application/json",
+        success:function (jsonResp) {
+            if(jsonResp.status===200){
+                loadCustomerTable(jsonResp.data);
+            }else if(jsonResp.status===404){
+                alert(jsonResp.message);
+            }else{
+                alert(jsonResp.data);
+            }
+        },
+        error:function (ob, textStatus, error) {
+            console.log(ob);
+            console.log(textStatus);
+            console.log(error);
+        }
+    });
+
+    function loadCustomerTable(data) {
+        tblCustomer.empty();
+
+        for (let i=0; i<data.length; i++){
+            let id=data[i].id;
+            let name=data[i].name;
+            let address=data[i].address;
+            let salary=data[i].salary;
+
+            let row = `<tr scope="row"><td>${i+1}</td><td><a href="#">${id}</a></td><td>${name}</td><td>${address}</td><td>${salary}</td></tr><tr class="spacer"><td colspan="100"></td></tr>`;
+            tblCustomer.append(row);
+        }
+    }
+}
+
+function validateCustId(){
+    if (cusIDRegEx.test(txtCusID.val())) {
+        txtCusID.css('border','3px solid green');
+    }else{
+        txtCusID.css('border','3px solid red');
+    }
+}
+function validateCustName(){
+    if (cusNameRegEx.test(txtCusName.val())) {
+        txtCusName.css('border','3px solid green');
+    }else{
+        txtCusName.css('border','3px solid red');
+    }
+}
+function validateCustAddress(){
+    if (cusAddressRegEx.test(txtCusAddress.val())) {
+        txtCusAddress.css('border','3px solid green');
+    }else{
+        txtCusAddress.css('border','3px solid red');
+    }
+}
+function validateCustContact(){
+    if (cusContactRegEx.test(txtCusSalary.val())) {
+        txtCusSalary.css('border','3px solid green');
+    }else{
+        txtCusSalary.css('border','3px solid red');
+    }
+}
 
 
 
