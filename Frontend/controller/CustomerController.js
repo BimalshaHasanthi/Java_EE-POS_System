dc04b195dcac52c13c7ec85b08e4d8fc59594362
BimalshaHasanthi <bimalshahasanthi@gmail.com>
2022-07-02 16:59:56 +0530

    Added customer update

--- Frontend/controller/CustomerController.js
+++ Frontend/controller/CustomerController.js
@@ -503,6 +503,38 @@ btnCusSave.click(function () {
 });
 
 
+btnCusUpdate.click(function () {
+
+    let jsonReq = {option : "",data : {id: txtCusID.val(),name: txtCusName.val(),address: txtCusAddress.val(),salary: txtCusSalary.val()}}
+
+    $.ajax({
+        url:"http://localhost:8080/Backend/customer",
+        method:"PUT",
+        contentType:"application/json",
+        data:JSON.stringify(jsonReq),
+        success:function (jsonResp) {
+            if(jsonResp.status===200){
+                alert(jsonResp.message);
+                loadAllCustomers();
+                setCustomerCombo();
+            }else if(jsonResp.status===404){
+                alert(jsonResp.message);
+            }else{
+                alert(jsonResp.data);
+            }
+        },
+        error:function (ob, textStatus, error) {
+            console.log(ob);
+            console.log(textStatus);
+            console.log(error);
+        }
+    });
+
+    loadFromCustomerTable();
+
+});
+
+
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
